4c5c59aa3049790c4d1ba51d4516fc205a14cb50
Romario Ramirez <rom23r@gmail.com>
2015-01-26 13:10:31 -0600

    Made some test of Real's speed.

---
+++ number/RealSpeedTest.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <ctime>
+#include "Real.h"
+
+using namespace std;
+
+int main()
+{
+  int sums = 1000000;
+  
+  clock_t t = clock();
+  for(int i = 0; i < sums; i++)
+    Real a = Real(1.2) + Real(1.2);
+  t = clock() - t;
+  cout << "Real Double time (" << sums  << " sums): " << t << " = " << ((float) t) / CLOCKS_PER_SEC << endl;
+
+  t = clock();
+  for(int i = 0; i < sums; i++)
+    Real b = Real(1,2) + Real(1,2);
+  t = clock() - t;
+  cout << "Real Fraction time (" << sums  << " sums): " << t << " = " << ((float) t) / CLOCKS_PER_SEC << endl;
+
+  t = clock();
+  for(int i = 0; i < sums; i++)
+    double c = 1.2 + 1.2;
+  t = clock() - t;
+  cout << "double time (" << sums  << " sums): " << t << " = " << ((float) t) / CLOCKS_PER_SEC << endl;
+  
+  return 0;
+}
